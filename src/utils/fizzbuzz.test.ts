41c9ee11d5afb8716f336e1c50d550188514b5c1
nchhujimiyama <t___yagi@outlook.jp>
2023-10-05 13:59:07 +0900

    add fizzbuzz

---
+++ src/utils/fizzbuzz.test.ts
@@ -0,0 +1,33 @@
+import { fizzbuzz } from "./fizzbuzz";
+
+const testData = [
+  {
+    testCase: "1の場合は1が返る",
+    value: 1,
+    expected: 1,
+  },
+  {
+    testCase: "3の場合はFizzが返る",
+    value: 3,
+    expected: "Fizz",
+  },
+  {
+    testCase: "5の場合はBuzzが返る",
+    value: 5,
+    expected: "Buzz",
+  },
+  {
+    testCase: "15の場合はFizzBuzzが返る",
+    value: 15,
+    expected: "FizzBuzz",
+  },
+];
+
+describe("FizzBuzz", () => {
+  testData.forEach((v) => {
+    test(v.testCase, () => {
+      const result = fizzbuzz(v.value);
+      expect(result).toBe(v.expected);
+    });
+  });
+});
---
+++ src/utils/fizzbuzz.ts
@@ -0,0 +1,10 @@
+export const fizzbuzz = (value: number) => {
+  if (value % 3 === 0 && value % 5 === 0) {
+    return "FizzBuzz";
+  } else if (value % 3 === 0) {
+    return "Fizz";
+  } else if (value % 5 === 0) {
+    return "Buzz";
+  }
+  return value;
+};
